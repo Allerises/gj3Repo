9b8dbcd17a3662ea2c68afa78fd35e572fbc6639
Will <williamburstein1@gmail.com>
2018-04-21 22:20:58 -0400

    Level Manager, Level Object, Very Basic Generation Properities, Basic Setup for Other Features

---
+++ New Unity Project/Assets/Scripts/Level.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public GameObject Generate(float distance) {
+		GameObject obj = Instantiate(this.gameObject, new Vector2(distance, 0), transform.rotation);
+		return obj;
+	}
+}
---
+++ New Unity Project/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public Level levelOne;
+	public Level levelTwo;
+	public Level levelThree;
+	public Level levelFour;
+	public Level levelFive;
+	public int levelSize = 30;
+	public int originX = 0;
+	private List<GameObject> currentLevel = new List<GameObject>();
+	private int currentLevelNumber = 0;
+	void Start () {
+		if (!(levelOne != null && levelTwo != null && levelThree != null && levelFour != null && levelFive != null)) {
+			Debug.Log ("All of the level prefabs must be set through the LevelManager GameObject!");
+			Application.Quit ();
+		}
+		LoadNextLevel();
+	}
+
+	void Update () {
+		
+	}
+
+	private void LoadNextLevel() {
+		float distance = originX;
+		for (int i=0; i < levelSize; i++) {
+			int selected = Random.Range (0, 6);
+			if (selected == 1) {
+				load (levelOne, distance);
+			} else if (selected == 2) {
+				load (levelTwo, distance);
+			} else if (selected == 3) {
+				load (levelThree, distance);
+			} else if (selected == 4) {
+				load (levelFour, distance);
+			} else if (selected == 5) {
+				load (levelFive, distance);
+			}
+		}
+	}
+
+	private void load(Level level, float distance) {
+		GameObject newLevel = level.Generate (distance);
+		distance += newLevel.transform.localScale.x;
+		currentLevel.Add (newLevel);
+	}
+
+	public void resetLevel() {
+		for (int i = 0; i < currentLevel.Length; i++) {
+			Destroy (currentLevel [i]);
+		}
+	}
+}
