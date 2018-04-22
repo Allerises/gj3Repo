416df7ac342db8549ce2168a6bd4db533b9b547a
Allerises <ibby.star.111@gmail.com>
2018-04-22 16:42:54 +0700

    Level generation works, rope spawning needs to be fixed

---
+++ New Unity Project/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GameObject.FindWithTag("MainCamera").GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.right * speed;
+    }
+}
---
+++ New Unity Project/Assets/Scripts/RopeSpawner.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeSpawner : MonoBehaviour
+{
+
+    public GameObject rope;
+
+    private Transform ropeT;
+    private Camera cam;
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
+        ropeT = rope.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 worldMouse = cam.ScreenToWorldPoint(mousePos);
+            worldMouse = new Vector3(worldMouse.x, worldMouse.y, 0f);
+            //Debug.Log(worldMouse);
+            Vector3 dir = worldMouse - transform.position;
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, dir, out hit))
+            {
+                if (hit.collider.tag == "Level")
+                {
+                    Vector3 pos = (hit.point + transform.position) / 2;
+                    Transform rope = Instantiate(ropeT, pos, Quaternion.identity);
+                    
+                }
+            }
+        }
+    }
+}
